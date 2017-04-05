8524d0dc2be4ee5aac4696e4a1460f5941e6679f
juanpale <juanpale@users.noreply.github.com>
2017-04-05 09:32:32 -0300

    Add files via upload

---
+++ CacheUtils.cs
@@ -0,0 +1,76 @@
+﻿using Apache.Ignite.Core;
+using Apache.Ignite.Core.Binary;
+using Apache.Ignite.Core.Cache;
+using Apache.Ignite.Core.Cache.Configuration;
+using Apache.Ignite.Core.Cache.Query;
+using Apache.Ignite.Core.Cache.Store;
+using Apache.Ignite.Core.Common;
+using Apache.Ignite.Core.Communication.Tcp;
+using Oracle.DataAccess.Client;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Net.NetworkInformation;
+using System.Threading;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace testIgnite
+{
+
+    
+    class CacheUtils
+    {
+
+
+        public static void startCache()
+        {
+
+            IgniteConfiguration cfg = new IgniteConfiguration
+            {
+                BinaryConfiguration = new BinaryConfiguration(typeof(TestObject), typeof(TestObjectPK))
+                ,CommunicationSpi = new TcpCommunicationSpi
+                {
+                    LocalPort = 4321   // Override local port.
+                }
+
+            };
+
+
+
+            using (var ignite = Ignition.Start(cfg))
+            {
+                
+                ICache<TestObjectPK, TestObject> cache = ignite.GetOrCreateCache<TestObjectPK, TestObject>(
+                    new CacheConfiguration("TestObject", new QueryEntity(typeof(TestObjectPK), typeof(TestObject))));
+                
+
+                TestObjectPK testObjectPK = new TestObjectPK();
+                testObjectPK.NAME = "h1";
+                testObjectPK.ID = 1;
+                TestObject testObject = new TestObject();
+                testObject.VALUE = "value1";
+                cache.Put(testObjectPK, testObject);
+                Console.WriteLine("==================I'm hear =====================================");
+               
+                
+
+                TestObjectPK findpk = new TestObjectPK();
+                findpk.NAME = "h1";
+                findpk.ID = 1;
+                var fieldsQuery = new SqlFieldsQuery("select _val,_key from TestObject where NAME=? and VALUE=?", "h1","value1");
+                
+
+                IQueryCursor<IList> queryCursor = cache.QueryFields(fieldsQuery);
+                foreach (IList fieldList in queryCursor)
+                    Console.WriteLine(((TestObject)fieldList[0]).VALUE);
+                
+            }
+
+        }
+
+
+
+    }
+}
---
+++ TestObject.cs
@@ -0,0 +1,100 @@
+﻿using Apache.Ignite.Core.Binary;
+using Apache.Ignite.Core.Cache.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testIgnite
+{
+
+    class TestObjectPK : IBinarizable
+    {
+        private Int32 id;
+        private String name;
+
+        [QuerySqlField]
+        public string NAME
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                this.name = value;
+            }
+        }
+
+        [QuerySqlField]
+        public Int32 ID
+        {
+            get
+            {
+                return this.id;
+            }
+            set
+            {
+                this.id = value;
+            }
+        }
+
+        public void WriteBinary(IBinaryWriter writer)
+        {
+            writer.WriteInt("ID", ID);
+            writer.WriteString("NAME", NAME);
+        }
+
+        public void ReadBinary(IBinaryReader reader)
+        {
+            ID = reader.ReadInt("ID");
+            NAME = reader.ReadString("NAME");
+        }
+
+        public override int GetHashCode()
+        {
+            return id.GetHashCode() + name.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            return id == ((TestObjectPK)obj).id && name == ((TestObjectPK)obj).NAME;
+        }
+    }
+
+
+    class TestObject : IBinarizable
+    {
+        private String value2;
+        [QuerySqlField]
+        public String VALUE
+        {
+            get { return this.value2; }
+            set { this.value2 = value; }
+        }
+
+        public void WriteBinary(IBinaryWriter writer)
+        {
+            writer.WriteString("VALUE", VALUE);
+        }
+
+        public void ReadBinary(IBinaryReader reader)
+        {
+            // Read order does not matter, however, reading in the same order 
+            // as writing improves performance          
+            VALUE = reader.ReadString("VALUE");
+        }
+
+        public override int GetHashCode()
+        {
+            return VALUE.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            return VALUE == ((TestObject)obj).VALUE;
+        }
+
+    }
+}
